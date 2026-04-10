df31133937354fdd552cfc7a3d1a6c00e11d0e33
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-10 02:45:44 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -1,17 +1,21 @@
 const SHAPES = [
-  [[1]],
-  [[1,1]],
-  [[1],[1]],
-  [[1,1,1]],
-  [[1],[1],[1]],
-  [[1,1],[1,1]],
-  [[1,0],[1,1]],
-  [[0,1],[1,1]]
+  [[1]], [[1,1]], [[1],[1]],
+  [[1,1,1]], [[1],[1],[1]],
+  [[1,1],[1,1]]
 ];
+
+const BONUS_TYPES = ['hammer','bomb','laser','star'];
 
 let pieces = [];
 
 function createPiece() {
+  if (Math.random() < 0.25) {
+    return {
+      bonus: BONUS_TYPES[Math.floor(Math.random()*BONUS_TYPES.length)],
+      used:false
+    };
+  }
+
   const shape = SHAPES[Math.floor(Math.random() * SHAPES.length)];
   return {
     matrix: shape,
